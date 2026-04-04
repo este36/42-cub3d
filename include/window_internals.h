147e7d3575929d39b6fcf4ac575af82587e8dace
este36 <mercierstban@gmail.com>
2026-04-04 20:23:06 +0200

    fix window_internal.h: norminette

--- include/window_internals.h
+++ include/window_internals.h
@@ -6,7 +6,7 @@
 /*   By: emercier <emercier@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 19:19:50 by emercier          #+#    #+#             */
-/*   Updated: 2026/04/04 14:34:42 by emercier         ###   ########.fr       */
+/*   Updated: 2026/04/04 20:22:20 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,14 @@ int		on_win_keyup(int ev, t_window *w);
 int		on_win_mousedown(int ev, int x, int y, t_window *w);
 int		on_win_mouseup(int ev, int x, int y, t_window *w);
 int		on_win_destroy(t_window *w);
-
 int		win_main_loop(t_window *w);
-
 void	win_init_hooks(t_window *w);
-
 void	update_fps(t_window *w);
 void	draw_text_queries(t_window *w);
 void	win_render_textboxes(t_window *w);
 void	textbox_calculate_dimensions(t_textbox *t);
 void	sleep_ms(int ms);
 void	retrieve_lines(t_darr *lines, t_str_ref *text, size_t max_width);
-
 
 typedef	struct s_text_query
 {
