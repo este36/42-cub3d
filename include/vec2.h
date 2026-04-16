ed8e0660a161462a706cdf18d964ff6cad9b0779
este36 <mercierstban@gmail.com>
2026-04-16 12:57:09 +0200

    feat: add more vec2 utilities

--- include/vec2.h
+++ include/vec2.h
@@ -6,7 +6,7 @@
 /*   By: emercier <emercier@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 12:04:21 by emercier          #+#    #+#             */
-/*   Updated: 2026/04/09 12:17:25 by emercier         ###   ########.fr       */
+/*   Updated: 2026/04/16 11:43:03 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,20 @@ typedef struct s_vec2
 }	t_vec2;
 
 t_vec2	vec2_add(t_vec2 a, t_vec2 b);
+t_vec2	vec2_mul(t_vec2 a, t_vec2 b);
 t_vec2	vec2_sub(t_vec2 a, t_vec2 b);
 t_vec2	vec2_scale(t_vec2 a, double scalar);
 double	vec2_len(t_vec2 a);
 t_vec2	vec2_rot(t_vec2 a, double angle);
 t_vec2	vec2_norm(t_vec2 a);
 double	vec2_dot(t_vec2 a, t_vec2 b);
+
+typedef struct s_point	t_point;
+t_vec2	vec2(double x, double y);
+t_point	vec2_to_point(t_vec2 a);
+t_vec2	point_to_vec2(t_point a);
+
+# define V2_FMT "{x=%f, y=%f}"
+# define V2_ARG(v) (v).x, (v).y
 
 #endif // VEC2_H
