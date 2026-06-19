76f8fb9d8342b3ad8f8864e10503c8a2e31089de
este36 <mercierstban@gmail.com>
2026-06-19 14:39:28 +0200

    fix: leaks in `main()` and SEGV in `free_game_data()`

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: emercier <emercier@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 19:02:47 by emercier          #+#    #+#             */
-/*   Updated: 2026/06/19 13:16:28 by emercier         ###   ########.fr       */
+/*   Updated: 2026/06/19 14:34:06 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	on_mouse_move(t_point prev, t_point curr, t_window *w)
 	return (0);
 }
 
-void	init(t_window *window, t_game *game)
+int	init_context(t_window *window, t_game *game)
 {
 	ft_bzero(game, sizeof(*game));
 	ft_bzero(window, sizeof(*window));
@@ -53,7 +53,14 @@ void	init(t_window *window, t_game *game)
 	window->main_loop = render;
 	window->mlx = mlx_init();
 	if (!window->mlx)
-		exit(1);
+		return (1);
+	return (0);
+}
+
+void destroy_context(t_window *window, t_game *game)
+{
+	free_game_data(game);
+	destroy_window(window);
 }
 
 int	main(int argc, char **argv)
@@ -67,18 +74,16 @@ int	main(int argc, char **argv)
 		printf("Usage: %s <*.cub path>\n", argv[0]);
 		return (1);
 	}
-	init(&window, &game);
-	if (!init_game(&game, argv[1]))
-		return (destroy_window(&window), 1);
+	if (init_context(&window, &game) != 0 || !init_game(&game, argv[1]))
+		return (destroy_context(&window, &game), 1);
 	if (create_window(&window) != 0)
-		return (1);
+		return (destroy_context(&window, &game), 1);
 	game.player.dir = (t_vec2){cos(game.player.angle), sin(game.player.angle)};
 	print_game_data(&game);
 	init_fov(&game);
 	if (compute_vignette(&game) != 0)
-		return (1);
+		return (destroy_context(&window, &game), 1);
 	show_window(&window);
-	free_game_data(&game);
-	destroy_window(&window);
+	destroy_context(&window, &game);
 	return (0);
 }
